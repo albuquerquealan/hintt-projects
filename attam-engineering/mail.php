6bcb6cdbe30bcc0da52f1e9174fc3ffb77eccf38
AlbuquerqueAlan <alan.nicolas@outlook.com>
2018-08-03 18:09:04 -0300

    correções site

--- attam-engineering/mail.php
+++ attam-engineering/mail.php
@@ -1,32 +1,31 @@
 <?php
-
-// Replace this with your own email address
-$siteOwnersEmail = ; //email de destino 
+// Replace this with com  own email address
+$siteOwnersEmail = '';
 if($_POST) {
-   $name = trim(stripslashes($_POST['contactName']));
-   $email = trim(stripslashes($_POST['contactEmail']));
-   $subject = "Contato via website";
-   $contact_message = trim(stripslashes($_POST['contactMessage']));
+   $name = trim(stripslashes($_POST['name']));
+   $email = trim(stripslashes($_POST['email']));
+   $subject = trim(stripslashes($_POST['subject']));
+   $contact_message = trim(stripslashes($_POST['message']));
    // Check Name
 	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
+		$error['name'] = "Por favor entre com seu nome.";
 	}
 	// Check Email
 	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Please enter a valid email address.";
+		$error['email'] = "Por favor entre com um e-mail válido.";
 	}
 	// Check Message
 	if (strlen($contact_message) < 15) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+		$error['message'] = "Por favor entre com sua message. Ela deverá ter mais de 15 caracteres.";
 	}
    // Subject
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+	if ($subject == '') { $subject = "myForm"; }
    // Set Message
-   $message .= "Email from: " . $name . "<br />";
-	$message .= "Email address: " . $email . "<br />";
-   $message .= "Message: <br />";
+   $message .= "<b> Enviado por: </b>  " . $name . "<br />";
+	$message .= "<b>E-mail:</b> " . $email . "<br />";
+   $message .= "<b>Mensagem:</b> <br /><br />";
    $message .= $contact_message;
-   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+   $message .= "<br /> ----- <br /> <b>Esse e-mail foi enviado pelo seu formulário de contato no seu site.</b> <br />";
    // Set From: header
    $from =  $name . " <" . $email . ">";
    // Email Headers
@@ -37,8 +36,8 @@ if($_POST) {
    if (!$error) {
       ini_set("sendmail_from", $siteOwnersEmail); // for windows server
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
-		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+		if ($mail) { echo "e-mail enviado com sucesso ! "; }
+      else { echo "Algo deu errado. Tende novamente."; }
 	} # end if - no validation error
 	else {
 		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
